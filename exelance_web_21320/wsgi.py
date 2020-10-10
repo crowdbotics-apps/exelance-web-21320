0832f81c5217519418f5282c636e39f629ec580b
Crowdbotics <team@crowdbotics.com>
2020-10-10 06:38:05 +0000

    Update models

--- exelance_web_21320/wsgi.py
+++ exelance_web_21320/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exelance_web_21320.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "exelance_web_21320.settings")
 
 application = get_wsgi_application()
